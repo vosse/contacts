5b9a21e4e467990dd1045908bf889b3caff4518c
vosse <jovicevicdev@gmail.com>
2020-06-01 22:42:51 +0200

    Updates

---
+++ precache-manifest.d98b8a8ee8cd1f7f15f364e8f5738bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71dd15f7a69745798572ce28bcc0cde1",
+    "url": "/contacts/index.html"
+  },
+  {
+    "revision": "56839b5f04e93f1c2b48",
+    "url": "/contacts/static/css/main.ae16f881.chunk.css"
+  },
+  {
+    "revision": "8e4f38db48f833dfc917",
+    "url": "/contacts/static/js/2.2c173069.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contacts/static/js/2.2c173069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56839b5f04e93f1c2b48",
+    "url": "/contacts/static/js/main.3cedd657.chunk.js"
+  },
+  {
+    "revision": "a0a7e12d79129e6dd91e",
+    "url": "/contacts/static/js/runtime-main.efc04f66.js"
+  },
+  {
+    "revision": "d99b5ea4b945f2aea91ed86eb159e249",
+    "url": "/contacts/static/media/agenda.d99b5ea4.svg"
+  }
+]);
